bf372332fee6c17db615644bd66c2014b2d3a6e8
ASRync <m_igor_r@mail.ru>
2022-04-17 23:11:11 +0700

    styled wrapper form

--- src/components/FormWrapper/FormWrapper.tsx
+++ src/components/FormWrapper/FormWrapper.tsx
@@ -1,12 +1,21 @@
+import styled from '@emotion/styled'
 import React, { FC } from 'react'
 
 interface FormProps {
     children: any
 }
 
+const Form = styled('form')({
+    background: '#0C53A6',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 20,
+    borderRadius: 10,
+})
+
 const FormWrapper: FC<FormProps> = ({children}) => {
     return (
-        <form>{children}</form>
+        <Form>{children}</Form>
     )
 }
 
