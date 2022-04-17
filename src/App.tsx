abd9ec4b0ac848a02d8aff3de9def5aa4381a754
ASRync <m_igor_r@mail.ru>
2022-04-17 22:59:29 +0700

    create styles for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Login from './pages/publicPages/Login';
 
+const AppWrapper = styled('div')({
+  display: 'grid',
+  gridTemplateColumns: '13% 1fr',
+  gridTemplateRows: '1fr',
+})
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <AppWrapper>
         <Sidebar />
         <Login />
-      </div>
+      </AppWrapper>
     </BrowserRouter>
   );
 }
