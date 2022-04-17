6574764e5f3def9a4e269a60f94479f028e9a36a
ASRync <m_igor_r@mail.ru>
2022-04-17 22:10:08 +0700

    create input component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import Login from './pages/publicPages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Sidebar />
+        <Login />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,10 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+const Input = () => {
+    return (
+        <TextField variant="filled" margin='normal' />
+    )
+}
+
+export default Input
