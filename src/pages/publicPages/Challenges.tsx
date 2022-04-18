bfdac5d5eb3693172029511458bf2823ef913102
ASRync <m_igor_r@mail.ru>
2022-04-18 16:27:28 +0700

    create challenges page

---
+++ src/pages/publicPages/Challenges.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import MainWrapper from '../../components/MainWrapper/MainWrapper'
+
+const Challenges: FC = () => {
+    return (
+        <MainWrapper>
+            <div>Challenges</div>
+        </MainWrapper>
+    )
+}
+
+export default Challenges
