d99c8077876398758c3fb0b240b9753a3165fe1f
ASRync <m_igor_r@mail.ru>
2022-04-18 16:28:30 +0700

    create settings page

---
+++ src/pages/privatePages/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainWrapper from '../../components/MainWrapper/MainWrapper'
+
+const Settings = () => {
+    return (
+        <MainWrapper>
+            <div>Settings</div>
+        </MainWrapper>
+    )
+}
+
+export default Settings
