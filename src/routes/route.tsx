7bdeca884a6b66201c01f179a2ea4ba4b412876d
ASRync <m_igor_r@mail.ru>
2022-04-18 16:34:00 +0700

    create private routes

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,7 +1,26 @@
+import Challenge from "../pages/privatePages/Challenge";
+import Settings from "../pages/privatePages/Settings";
+import Challenges from "../pages/publicPages/Challenges";
 import Login from "../pages/publicPages/Login";
 import Signup from "../pages/publicPages/Signup";
 
 export const publicRoutes = [
     {path: '/login', element: <Login />},
-    {path: '/signup', element: <Signup />}
+    {path: '/signup', element: <Signup />},
+    {path: '/challenge', element: <Challenges />}
+]
+
+export const privateRoutes = [
+    {
+        path: '/challenges', 
+        element: <Challenges />, 
+        children: {
+            path: '/challenges/:id',
+            element: <Challenge />
+        }
+    },
+    {
+        path: '/settings',
+        element: <Settings />
+    }
 ]
