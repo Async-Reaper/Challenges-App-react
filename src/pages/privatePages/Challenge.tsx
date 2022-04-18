e2e73248fd2181042977b346b845ffda959ea176
ASRync <m_igor_r@mail.ru>
2022-04-18 16:29:50 +0700

    create challenge by id page

---
+++ src/pages/privatePages/Challenge.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainWrapper from '../../components/MainWrapper/MainWrapper'
+
+const Challenge = () => {
+    return (
+        <MainWrapper>
+            <div>
+                Challenge
+            </div>
+        </MainWrapper>
+    )
+}
+
+export default Challenge
