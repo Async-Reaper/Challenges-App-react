b8f9fb8a6eb82197c8e8ade33a0e9b828d17abb8
ASRync <m_igor_r@mail.ru>
2022-04-18 16:18:54 +0700

    create public routes

---
+++ src/routes/route.tsx
@@ -0,0 +1,8 @@
+import Login from "../pages/publicPages/Login";
+import Signup from "../pages/publicPages/Signup";
+
+export const publicRoutes = [
+    {path: '/login', element: <Login />},
+    {path: '/signup', element: <Signup />}
+]
+
