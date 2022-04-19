4747cd5d792005f56d053f2a3e792e4854078e9b
ASRync <m_igor_r@mail.ru>
2022-04-19 20:19:10 +0700

    create constants urls

---
+++ src/constants/URL.ts
@@ -0,0 +1,3 @@
+export const urlDomain = 'http://bahbka.pythonanywhere.com/';
+export const urlGetChallenges =  'challenges/get_challenges_list/'
+export const urlLogin =  'users/login'
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { IChallenge } from '../models/IChallenge'
-
+import { urlDomain, urlGetChallenges } from '../constants/URL'
 
 export const postApi = createApi({
     reducerPath: 'getAllChallenges',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://bahbka.pythonanywhere.com/'}),
+    baseQuery: fetchBaseQuery({ baseUrl: urlDomain}),
     tagTypes: ['Challenge'],
     endpoints: (build) => ({
         getAllChallenges: build.query<IChallenge[], string>({
             query: () => ({
-                url: 'challenges/get_challenges_list/',
+                url: urlGetChallenges,
             }),
             providesTags: res => ['Challenge']
         })
