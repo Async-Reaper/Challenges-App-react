8e3a490e8fa3c88d347ef567ea8da40ab6958f50
ASRync <m_igor_r@mail.ru>
2022-04-20 14:58:44 +0700

    create request for signup

---
+++ src/models/ISingupAnswer.ts
@@ -0,0 +1,7 @@
+export interface ISignupAnswer {
+    first_name: string;
+	surname: string;
+	username: string;
+    email: string;
+	message: string
+}
---
+++ src/services/SignupService.ts
@@ -0,0 +1,24 @@
+import { AppDispatch } from "../store/store"
+import { IUserSignup } from "../models/IUserSignup"
+import { signupFetch, signupFetchError, signupFetchSuccess } from "../store/reducers/signupSlice"
+import { urlDomain, urlSignup } from "../constants/URL"
+import { ISignupAnswer } from "../models/ISingupAnswer"
+
+
+export const signupUser = (data: IUserSignup) => {
+    return async (dispatch: AppDispatch) => {
+        try {
+            dispatch(signupFetch())
+            
+            const response = await fetch( urlDomain + urlSignup, {
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+
+            const res: ISignupAnswer = await response.json();
+            dispatch(signupFetchSuccess(res.message))
+        } catch (error) {
+            dispatch(signupFetchError('error'))
+        }
+    }
+}
