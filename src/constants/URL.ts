4c0bf446769ff4c966156103cf75a73c67624345
ASRync <m_igor_r@mail.ru>
2022-04-20 21:38:51 +0700

    create constant url for challenge by id

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -2,4 +2,5 @@ export const urlDomain = 'http://bahbka.pythonanywhere.com/';
 export const urlGetChallenges =  'challenges/get_challenges_list/';
 export const urlLogin = 'users/login/';
 export const urlSignup = 'users/signup/';
-export const urlSignout = 'users/logout/'
+export const urlSignout = 'users/logout/';
+export const urlGetChallengeById = 'challenges/get_detail_challenge/'
