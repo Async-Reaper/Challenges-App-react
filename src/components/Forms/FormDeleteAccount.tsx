e4de33ce85b8aba7215abc1e0105cd666f10f265
ASRync <m_igor_r@mail.ru>
2022-04-20 23:15:02 +0700

    create component for delete account

---
+++ src/components/Forms/FormDeleteAccount.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React, { FC } from 'react'
+
+const FormDeleteAccount: FC = () => {
+    return (
+        <div>
+            <Button type='submit' variant="contained">Change email</Button>
+        </div>
+    )
+}
+
+export default FormDeleteAccount
