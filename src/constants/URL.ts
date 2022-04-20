fa84f111f0f9698207c01669a48b0061b68d97d9
ASRync <m_igor_r@mail.ru>
2022-04-20 22:54:15 +0700

    create constants url change password

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -1,6 +1,9 @@
 export const urlDomain = 'http://bahbka.pythonanywhere.com/';
+
 export const urlGetChallenges =  'challenges/get_challenges_list/';
+export const urlGetChallengeById = 'challenges/get_detail_challenge/'
+
 export const urlLogin = 'users/login/';
 export const urlSignup = 'users/signup/';
 export const urlSignout = 'users/logout/';
-export const urlGetChallengeById = 'challenges/get_detail_challenge/'
+export const urlChangePassword = 'users/change_user_password/'
