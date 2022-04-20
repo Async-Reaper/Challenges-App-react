52627b9151b7d6a133c82c1a2ad647b8dc2b21f3
ASRync <m_igor_r@mail.ru>
2022-04-20 23:16:07 +0700

    add form delete account in accordion

--- src/pages/privatePages/Settings.tsx
+++ src/pages/privatePages/Settings.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import FormChangeEmail from '../../components/Forms/FormChangeEmail';
 import FormChangePassword from '../../components/Forms/FormChangePassword';
+import FormDeleteAccount from '../../components/Forms/FormDeleteAccount';
 import AccordionSettings from '../../components/UI/Accordion/AccordionSettings';
 import MainWrapper from '../../components/UI/MainWrapper/MainWrapper';
 
@@ -9,6 +10,7 @@ const Settings: FC = () => {
         <MainWrapper>
             <AccordionSettings name='Change password' params={<FormChangePassword />}/>
             <AccordionSettings name='Change email' params={<FormChangeEmail />}/>
+            <AccordionSettings name='Delete account' params={<FormDeleteAccount />}/>
         </MainWrapper>
     )
 }
