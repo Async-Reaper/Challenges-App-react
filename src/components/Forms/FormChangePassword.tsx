91bd968b4ab6cc8c3d673d9a2d3d6654e53311a2
ASRync <m_igor_r@mail.ru>
2022-04-20 22:51:04 +0700

    create components for change password

---
+++ src/components/Forms/FormChangePassword.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react'
+import { IChangePassword } from '../../models/IChangePassword'
+import FormWrapper from '../UI/FormWrapper/FormWrapper'
+import Input from '../UI/Input/Input'
+
+const FormChangePassword: FC = () => {
+
+    const handleChangePassword = (e: React.MouseEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
+    const changeNewPassword: IChangePassword = {
+        old_password: '',
+        new_password: '',
+        new_password2: ''
+    }
+    return (
+        <FormWrapper method='PUT' onSubmit={e => handleChangePassword(e)}>
+            <Input label='Old password' onChange={e => changeNewPassword.old_password = e.target.value}/>
+            <Input label='New password' onChange={e => changeNewPassword.new_password = e.target.value}/>
+            <Input label='New password' onChange={e => changeNewPassword.new_password2 = e.target.value}/>
+        </FormWrapper>
+    )
+}
+
+export default FormChangePassword
