3852d50699c992e19718f16bc7b5520fe2c2bb89
ASRync <m_igor_r@mail.ru>
2022-04-21 00:11:40 +0700

    add handle for delete account in component

--- src/components/Forms/FormDeleteAccount.tsx
+++ src/components/Forms/FormDeleteAccount.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@mui/material'
 import React, { FC } from 'react'
+import { useTypedDispatch } from '../../hooks/useTypedDispatch'
+import { deleteAccount } from '../../services/SettingsService'
 
 const FormDeleteAccount: FC = () => {
+    const dispatch = useTypedDispatch()
     return (
         <div>
-            <Button type='submit' variant="contained">Delete account</Button>
+            <Button type='submit' variant="contained" onClick={() => dispatch(deleteAccount())}>Delete account</Button>
         </div>
     )
 }
