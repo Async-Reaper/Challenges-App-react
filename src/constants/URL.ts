75e3fe6bd22657089211a44e4d7679215cdb495b
ASRync <m_igor_r@mail.ru>
2022-04-21 21:06:31 +0700

    create url for get challenge members

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -2,7 +2,8 @@ export const urlDomain = 'http://bahbka.pythonanywhere.com/';
 
 export const urlGetChallenges =  'challenges/get_challenges_list/';
 export const urlGetChallengeById = 'challenges/get_detail_challenge/'
-export const urlCreateChallenge = 'challenges/create_challenge'
+export const urlCreateChallenge = 'challenges/create_challenge/'
+export const urlGetChallengeMembers = ' challenges/get_challenge_members/'
 
 export const urlLogin = 'users/login/';
 export const urlSignup = 'users/signup/';
