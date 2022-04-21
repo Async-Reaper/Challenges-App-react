ad1e07a283614b1c911b9f31f39766b757cd8d30
ASRync <m_igor_r@mail.ru>
2022-04-21 15:54:10 +0700

    add handle onchange for inputs

--- src/components/Forms/FormCreateChallenge.tsx
+++ src/components/Forms/FormCreateChallenge.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@mui/material'
 import React, { FC } from 'react'
+import { ICreateChallenge } from '../../models/ICreateChallenge'
 import FormWrapper from '../UI/FormWrapper/FormWrapper'
 import Input from '../UI/Input/Input'
 
 const FormCreateChallenge: FC = () => {
-    const newChallene = {
+
+    const newChallenge: ICreateChallenge = {
         name: '',
         finish_datetime: '',
         goal: '',
@@ -12,13 +14,29 @@ const FormCreateChallenge: FC = () => {
         requirements: '',
         bet: 0
     }
+
     return (
         <FormWrapper method='POST' onSubmit={}>
-            <Input label='Name'/>
-            <Input label='Finish datetime'/>
-            <Input label='Goal'/>
-            <Input label='Description'/>
-            <Input label='Requirements'/>
+            <Input 
+                label='Finish datetime'
+                onChange={e => newChallenge.finish_datetime = e.target.value}
+            />
+            <Input 
+                label='Name'
+                onChange={e => newChallenge.name = e.target.value}
+            />
+            <Input 
+                label='Goal'
+                onChange={e => newChallenge.goal = e.target.value}
+            />
+            <Input 
+                label='Description'
+                onChange={e => newChallenge.description = e.target.value}
+            />
+            <Input 
+                label='Requirements'
+                onChange={e => newChallenge.requirements = e.target.value}
+            />
             <Button type='submit' variant="contained">Create challenge</Button>
         </FormWrapper>
     )
