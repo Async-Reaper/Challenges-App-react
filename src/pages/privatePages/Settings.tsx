85ef101bb8937ef87e530d52e490f9327e6bb454
ASRync <m_igor_r@mail.ru>
2022-04-22 19:08:54 +0700

    fix import in setting component

--- src/pages/privatePages/Settings.tsx
+++ src/pages/privatePages/Settings.tsx
@@ -3,7 +3,6 @@ import SettingsAccordion from '../../components/Settings/SettingsAccordion';
 import Loader from '../../components/UI/Loader/Loader';
 import MainWrapper from '../../components/UI/MainWrapper/MainWrapper';
 import Popup from '../../components/UI/Popup/Popup';
-import PopupError from '../../components/UI/Popup/PopupError'
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 
 const Settings: FC = () => {
